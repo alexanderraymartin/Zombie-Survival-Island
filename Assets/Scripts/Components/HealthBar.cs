03b63478a9719d9d6753222d3ea686d4b4734c79
Alex Martin <alexanderraymartin@gmail.com>
2018-05-07 04:06:22 -0700

    Started implementing health bars

--- Assets/Scripts/Components/HealthBar.cs
+++ Assets/Scripts/Components/HealthBar.cs
@@ -6,12 +6,28 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Health))]
 public class HealthBar : MonoBehaviour
 {
-    public Image healthBar;
+    public GameObject healthBarPrefab;
+
+    private GameObject UI;
+    private GameObject healthBar;
+    private Health health;
+
+    void Start()
+    {
+        UI = GameObject.FindGameObjectWithTag("UI");
+        health = GetComponent<Health>();
+        healthBar = Instantiate(healthBarPrefab);
+        healthBar.transform.SetParent(UI.transform, false);
+    }
 
     void Update()
     {
-        Vector3 healthPosition = Camera.main.WorldToScreenPoint(this.transform.position);
-        healthBar.transform.position = healthPosition;
-        healthBar.fillAmount = GetComponent<Health>().currentHealth / GetComponent<Health>().maxHealth;
+        if (!health.isAlive)
+        {
+            Destroy(gameObject);
+        }
+
+        healthBar.transform.position = Camera.main.WorldToScreenPoint(this.transform.position);
+        //healthBar.fillAmount = GetComponent<Health>().currentHealth / GetComponent<Health>().maxHealth;
     }
 }
