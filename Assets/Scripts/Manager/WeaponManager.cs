046b383c88d75439b6f625b07ca45a650545151b
Alex Martin <alexanderraymartin@gmail.com>
2018-05-09 03:05:37 -0700

    Small fixes

--- Assets/Scripts/Manager/WeaponManager.cs
+++ Assets/Scripts/Manager/WeaponManager.cs
@@ -11,16 +11,13 @@ public class WeaponManager : NetworkBehaviour
     [Command]
     public void CmdEquipWeapon(GameObject gun)
     {
-        if (!isLocalPlayer || gun == null )
+        if (!isLocalPlayer || gun == null)
         {
             return;
         }
-        
-        gun.transform.SetParent(weaponHolder.transform);
-        gun.transform.localPosition = new Vector3(0, 0, 0);
-        gun.transform.localRotation = Quaternion.identity;
-        gun.GetComponent<Rigidbody>().isKinematic = true;
-        gun.GetComponent<Gun>().cam = gameObject.GetComponent<Player_Network>().firstPersonCharacter;
+
+        EquipWeapon(gun.GetComponent<NetworkIdentity>().netId);
+        RpcEquipWeapon(gun.GetComponent<NetworkIdentity>().netId);
     }
 
     [Command]
@@ -30,10 +27,39 @@ public class WeaponManager : NetworkBehaviour
         {
             return;
         }
-        
-        gun.transform.SetParent(null);
-        gun.GetComponent<Gun>().cam = null;
-        gun.GetComponent<Rigidbody>().isKinematic = false;
+
+        UnequipWeapon(gun.GetComponent<NetworkIdentity>().netId);
+        RpcUnequipWeapon(gun.GetComponent<NetworkIdentity>().netId);
+    }
+
+    [ClientRpc]
+    void RpcEquipWeapon(NetworkInstanceId weaponId)
+    {
+        EquipWeapon(weaponId);
+    }
+
+    void EquipWeapon(NetworkInstanceId weaponId)
+    {
+        GameObject weapon = ClientScene.FindLocalObject(weaponId);
+        weapon.transform.SetParent(weaponHolder.transform);
+        weapon.transform.localPosition = Vector3.zero;
+        weapon.transform.localRotation = Quaternion.identity;
+        weapon.GetComponent<Rigidbody>().isKinematic = true;
+        weapon.GetComponent<Gun>().cam = gameObject.GetComponent<Player_Network>().firstPersonCharacter;
+    }
+
+    [ClientRpc]
+    void RpcUnequipWeapon(NetworkInstanceId weaponId)
+    {
+        UnequipWeapon(weaponId);
+    }
+
+    void UnequipWeapon(NetworkInstanceId weaponId)
+    {
+        GameObject weapon = ClientScene.FindLocalObject(weaponId);
+        weapon.transform.SetParent(null);
+        weapon.GetComponent<Gun>().cam = null;
+        weapon.GetComponent<Rigidbody>().isKinematic = false;
     }
 
     void Update()
