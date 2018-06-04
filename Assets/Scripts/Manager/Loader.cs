aabb8792ffc2823be591d719caf64a3f95d335f4
Alex Martin <alexanderraymartin@gmail.com>
2018-06-04 03:10:01 -0700

    Fixed managers

--- Assets/Scripts/Manager/Loader.cs
+++ Assets/Scripts/Manager/Loader.cs
@@ -6,11 +6,16 @@ using UnityEngine.Networking;
 public class Loader : NetworkBehaviour
 {
     public GameObject gameManager;
+    public GameObject spawner;
 
     [ServerCallback]
     void Awake()
     {
-        Instantiate(gameManager);
+        GameObject gameManagerInstance = Instantiate(gameManager);
+        NetworkServer.Spawn(gameManagerInstance);
+
+        GameObject spawnerInstance = Instantiate(spawner);
+        NetworkServer.Spawn(spawnerInstance);
     }
 
 }
