68d84cfdf169b5579124e46155c3396d53dc761b
Nick Spencer <nicholas.j.spencer1@gmail.com>
2018-06-04 20:39:39 -0700

    music added

---
+++ Assets/DownloadedAssets/Lobby/Scripts/Lobby/LobbyMusic.cs
@@ -0,0 +1,47 @@
+﻿using Prototype.NetworkLobby;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyMusic : MonoBehaviour {
+    public AudioClip lobbyClip;
+    public AudioClip gameClip;
+
+    public float lobbyVolume;
+    public float gameVolume;
+
+    private AudioSource source;
+    private LobbyManager lobbyManager;
+
+    private bool isInGame;
+	// Use this for initialization
+	void Start () {
+        lobbyManager = GetComponent<LobbyManager>();
+        source = GetComponent<AudioSource>();
+        source.volume = lobbyVolume;
+        source.loop = true;
+        source.Play();
+        isInGame = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+		if (lobbyManager.topPanel.isInGame && !isInGame)
+        {
+            source.Stop();
+            source.volume = gameVolume;
+            source.clip = gameClip;
+            source.Play();
+            isInGame = true;
+        }
+        else if (!lobbyManager.topPanel.isInGame && isInGame)
+        {
+            source.Stop();
+            source.volume = lobbyVolume;
+            source.clip = lobbyClip;
+            source.Play();
+            isInGame = false;
+        }
+	}
+}
