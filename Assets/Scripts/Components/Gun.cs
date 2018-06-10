b601aa1981aa284705bea0a8c046ed16062250f4
Nick Spencer <nicholas.j.spencer1@gmail.com>
2018-06-10 14:41:16 -0700

    Gun Kickback

--- Assets/Scripts/Components/Gun.cs
+++ Assets/Scripts/Components/Gun.cs
@@ -7,6 +7,8 @@ using UnityEngine.Networking;
 [RequireComponent(typeof(WeaponGraphics))]
 public class Gun : NetworkBehaviour
 {
+    public GameObject firstPersonCharacter;
+
     public int reloadingSoundIndex;
     public int shootingSoundIndex;
     public bool isAuto;
@@ -15,11 +17,15 @@ public class Gun : NetworkBehaviour
     public float range;
     public float bulletPenetration; // 0 - 100
     public float reloadTime;
+    public float maxRecoil_x;
+    public float recoilSpeed;
+    public float recoilDelta;
+    public float sidewaysRecoil;
 
     public Vector3 hipFireLoc;
     public Vector3 sightFireLoc;
     public int aimSpeed;
-    private bool isAiming = false; 
+    private bool isAiming = false;
 
     [SyncVar]
     public int clipMaxAmmo;
@@ -46,12 +52,21 @@ public class Gun : NetworkBehaviour
     [HideInInspector]
     public Player_Network gunOwner;
 
+    private Camera playerCamera;
+    private Transform playerTransform;
+    private bool recoiling = false;
+    private float recoil = 0.0f;
+
+
+    Quaternion beforeRecoil;
+    Quaternion nextRecoil;
 
     [ServerCallback]
     void Awake()
     {
         clipAmmo = clipMaxAmmo;
         reserveAmmo = reserveMaxAmmo;
+
     }
 
     void OnEnable()
@@ -66,6 +81,10 @@ public class Gun : NetworkBehaviour
         {
             return;
         }
+
+        //playerCamera = gunOwner.transform.Find("FirstPersonCharacter").GetComponent<Camera>();
+        playerCamera = Camera.main;
+
 
         // Limit rate of fire
         if (Time.time >= nextTimeToFire)
@@ -87,6 +106,7 @@ public class Gun : NetworkBehaviour
 
         // Play shooting sound
         gunOwner.soundManager.PlaySound(shootingSoundIndex, transform.position, 0.15f);
+
 
         // Show muzzle flash
         gunOwner.weaponManager.MuzzleFlash();
@@ -113,7 +133,40 @@ public class Gun : NetworkBehaviour
             }
         }
 
+        if (recoil < 0)
+        {
+            recoil = recoilDelta;
+            beforeRecoil = playerCamera.transform.localRotation;
+        }
+        else
+        {
+            recoil += recoilDelta;
+        }
+        nextRecoil = beforeRecoil * Quaternion.Euler(0, Random.value * sidewaysRecoil - sidewaysRecoil / 2, 0);
+
         CheckForReload();
+    }
+
+    public void Update()
+    {
+        playerCamera = Camera.main;
+
+        if (recoil > 0)
+        {
+            //var maxRecoil = nextRecoil * Quaternion.Euler(maxRecoil_x, 0, 0);
+            var maxRecoil = nextRecoil * Quaternion.Euler(maxRecoil_x, 0, 0);
+
+            // Dampen towards the target rotation
+            playerCamera.transform.localRotation = Quaternion.Slerp(playerCamera.transform.localRotation, maxRecoil, Time.deltaTime * recoilSpeed / 6);
+        }
+        else if (recoil > -recoilDelta * 3)
+        {
+            var minRecoil = playerCamera.transform.localRotation;
+            // Dampen towards the target rotation
+            playerCamera.transform.localRotation = Quaternion.Slerp(playerCamera.transform.localRotation, beforeRecoil, Time.deltaTime * recoilSpeed / 2);
+        }
+
+        recoil -= Time.deltaTime;
     }
 
     public IEnumerator Reload()
